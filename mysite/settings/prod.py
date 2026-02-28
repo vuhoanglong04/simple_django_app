4989cf797aebe585a7ef9fa19292a8319fb518c2
Vu hoang long <longvulinhhoang@gmail.com>
2026-02-28 19:27:27 +0700

    Seperated setting env

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -61,10 +61,6 @@ DATABASES = {
             'PASSWORD': os.environ.get('DB_PASSWORD'),
             'HOST': os.environ.get('DB_HOST'),
             'PORT': os.environ.get('DB_PORT'),
-        },
-        'test': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
         }
 }
 
